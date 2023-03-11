e283923134e01f1ba9fb40a9cf06933f3b14de1a
Andreas Sünder <asuender@student.tgm.ac.at>
2023-03-11 22:24:23 +0100

    Update M8c.2
    Add checkTipp method and tests

--- M8c.2/lotto.py
+++ M8c.2/lotto.py
@@ -1,3 +1,5 @@
+import random, re
+
 def checkTipp(zahlen: list, tipps: list):
     if len(zahlen) != 7 or len(tipps) != 6:
         raise ValueError("Die Listen habeb eine ungültige Länge.")
@@ -6,6 +8,23 @@ def checkTipp(zahlen: list, tipps: list):
     for i in range(6):
         if zahlen[i] in tipps:
             treffer += 1
+
+    if treffer < 3:
+        return "Keine Treffer"
+    
+    nachricht = f"{treffer} übereinstimmende Treffer"
+
+    if zahlen[6] in tipps:
+        nachricht += " + Zusatzzahl"
+
     return treffer
 
-checkTipp([1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6])
+if __name__ == "__main__":
+    zahlen = [random.randint(1, 49) for i in range(7)]
+
+    print("Tipp eingeben:", end=" ")
+    input = input()
+    tipps = [int(i) for i in re.findall(r"\d+", input)]
+    
+    print(f"Die Zahlen waren: {zahlen}")
+    print(checkTipp(zahlen, tipps))
