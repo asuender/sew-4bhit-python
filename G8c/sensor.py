1f2aef719604e85a4a2a424f9b7e7cfccacdcf6a
asuender <asuender@student.tgm.ac.at>
2023-03-20 13:27:32 +0100

    Remove unnecessary constructor

--- G8c/sensor.py
+++ G8c/sensor.py
@@ -2,8 +2,5 @@ from random import randint
 from reading import Reading
 
 class Sensor:
-    def __init__(self):
-        pass
-    
     def get_reading(self) -> Reading:
         return Reading(randint(-20, 50), randint(100, 200))
