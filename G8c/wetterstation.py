818b9595cdb410aac0383ee9d0cd644e916cff83
asuender <asuender@student.tgm.ac.at>
2023-03-20 14:12:05 +0100

    Comment wetterstation class

--- G8c/wetterstation.py
+++ G8c/wetterstation.py
@@ -1,17 +1,36 @@
 from sensor import Sensor
 
 class Wetterstation:
+    """
+    Repräsentiert eine Wetterstation. Jede Station braucht einen Sensor,
+    von welchem Daten bezogen werden können.
+    
+    Des Weiteren werden zwei Methoden angeboten, um die
+    Durchschnittstemperatur bzw. -druck auszurechnen.
+    """
+
     def __init__(self, sensor: Sensor):
+        """
+        Initialisiert eine Wetterstation.
+
+        Parameter:
+        sensor: Sensor
+        """
+
         self._sensor = sensor
         self._readings = []
 
     def record_reading(self):
+        """Holt sich eine Aufzeichnung vom Sensor"""
+
         reading = self._sensor.get_reading()
         if len(self._readings):
             self._readings.pop(0)
         self._readings.append(reading)
 
     def avg_temperature(self):
+        """Berechnet die Durchschnittstemperatur aller Aufzeichnungen"""
+
         if len(self._readings) == 0:
             raise ValueError("Es sind keine Daten vorhanden!")
         
@@ -21,6 +40,8 @@ class Wetterstation:
         return sum / len(self._readings)
 
     def avg_pressure(self):
+        """Berechnet den Durschnittsdruck aller Aufzeichnungen"""
+
         if len(self._readings) == 0:
             raise ValueError("Es sind keine Daten vorhanden!")
         
